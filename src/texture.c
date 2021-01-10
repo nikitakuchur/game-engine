eebd17c5c1ce687413ee606f99d6fa5044812559
Nikita Kuchur <nikitakuchur@gmail.com>
2021-01-10 07:27:24 +0300

    Fixed stb_image cmake config

--- src/texture.c
+++ src/texture.c
@@ -2,8 +2,6 @@
 
 #include <glad/glad.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-
 #include <stb_image.h>
 
 uint32_t texture_create_empty() {
@@ -35,7 +33,7 @@ uint32_t texture_load(const char *filename, uint32_t format) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     stbi_set_flip_vertically_on_load(1);
